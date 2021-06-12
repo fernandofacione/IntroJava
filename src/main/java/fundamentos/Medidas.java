18c8c8039c7fa520c61016ee670886f63fd2c448
fernandofacione <FeR_1980__>
2021-06-12 13:48:25 -0300

    Aula 12/06

--- src/main/java/fundamentos/Medidas.java
+++ src/main/java/fundamentos/Medidas.java
@@ -9,13 +9,10 @@ public class Medidas {
     // 3.1 - Atributos
     static Scanner entrada = new Scanner(System.in);
 
-
     // 3.2 - Métodos e Funções
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String opcao;
-        int area = 0; // receber o resultado dos cálculos de área
-
-        //entrada = new Scanner(System.in); //instanciar o objeto de leitura do console
+        int area = 0; // receber o resultado dos cálculos de áreas
 
         System.out.println("Escolha o Calculo Desejado");
         System.out.println("(1) - Area do Quadrado");
@@ -23,26 +20,28 @@ public class Medidas {
         System.out.println("(3) - Area do Triangulo");
         System.out.println("(4) - Area do Circulo");
 
-        opcao = entrada.nextLine(); //leitura da opção
-        switch (opcao){
+        opcao = entrada.nextLine(); // leitura da opção
+        switch (opcao) {
             case "1":
                 area = calcularAreaDoQuadrado();
                 break;
+            case "2":
+                // ToDo: calcular área do retangulo
+                break;
             default:
-                throw new IllegalStateException("Valor invalido: " + opcao);
+                System.out.println("Opcao Invalida: " + opcao);
         }
-
-        System.out.println("A area e de " + area + "m²");
-
+        if (area > 0) {
+            System.out.println("A área é de " + area + "m²");
+        }
     }
+
     public static int calcularAreaDoQuadrado() {
 
         int lado; // tamanho do lado do quadrado
 
         System.out.println("Digite o tamanho do lado: ");
-        lado = entrada.nextInt(); //leitura do tamanho do lado
-        return lado * lado; // retorna a área do quadrants
-
+        lado = entrada.nextInt(); // leitura do tamanho do lado
+        return lado * lado; // retorna a área do quadrado
     }
-
 }
